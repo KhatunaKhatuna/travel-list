6e75b10b633fc5f5ead7558917b7b2cf3464fbbb
Khatuna Chitishvili <chitishvili.kh@gmail.com>
2023-09-10 16:08:11 +0400

    Add states for form elements Add handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,18 +20,46 @@ export default function App() {
 function Logo() {
   return <h1> 🌏️ Far Away ✈️</h1>;
 }
+
 function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { quantity, description, packed: false, id: Date.now() };
+    console.log(newItem);
+    setQuantity(1);
+    setDescription("");
+  }
+  // function hendleChangeSellect(e) {
+  //   console.log(e.target.value);
+  // }
+
+  // function hendleChangeinput(e) {
+  //   const inputValue = e.target.value;
+  //   return setDescription(inputValue);
+  // }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3> 🤔 What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
       <button>Add</button>
     </form>
   );
